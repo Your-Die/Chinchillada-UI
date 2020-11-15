7de804e3c48a0cd5e175a6cdc00e799e3844f6a2
Jordy <jordy_van_opstal@live.nl>
2020-11-15 21:49:07 +0100

    Implement Decorating Selectable
    Activates it's decorator when it is selected.

---
+++ Runtime/DecoratingSelectable.cs
@@ -0,0 +1,21 @@
+﻿using Chinchillada.Foundation;
+using Chinchillada.Foundation.UI;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Packages.Runtime
+{
+    public class DecoratingSelectable : ChinchilladaBehaviour, ISelectHandler, IDeselectHandler
+    {
+        [SerializeField] private ITextDecorator decorator;
+        
+        [SerializeField, Required, FindComponent]
+        private TMPPresenter presenter;
+
+
+        public void OnSelect(BaseEventData eventData) => this.presenter.AddDecorator(this.decorator);
+
+        public void OnDeselect(BaseEventData eventData) => this.presenter.RemoveDecorator(this.decorator);
+    }
+}
