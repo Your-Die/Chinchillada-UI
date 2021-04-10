8fe793e774226c7ae1283da133193043a39301d4
jordy <jordy_van_opstal@live.nl>
2021-04-11 01:21:42 +0200

    Add presenter visualizer wrapper

---
+++ Runtime/Presenters/PresenterVisualizer.cs
@@ -0,0 +1,11 @@
+namespace Chinchillada.Foundation.UI
+{
+    using Sirenix.Serialization;
+
+    public class PresenterVisualizer<T> : IVisualizer<T>
+    {
+        [OdinSerialize] private IPresenter<T> presenter;
+        
+        public void Visualize(T content) => this.presenter.Present(content);
+    }
+}
